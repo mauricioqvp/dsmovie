b05f5c71752bcd5bc294f389c31b55ced5b414eb
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2022-01-14 10:08:39 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
 import MovieCard from "assets/components/MovieCard";
 import Pagination from "assets/components/Pagination";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
             <Pagination />
